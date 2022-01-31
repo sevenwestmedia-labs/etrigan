985817aa8e87af631db11cd642044d2f9a79ad38
nicholas-yong <40938341+nicholas-yong@users.noreply.github.com>
2022-01-31 14:34:20 +0800

    Added sampling override test
    As per title.

--- packages/logging/src/logging-middleware.test.ts
+++ packages/logging/src/logging-middleware.test.ts
@@ -1,262 +1,198 @@
 import pino from 'pino'
 
 import { expressRequestLoggingMiddleware } from './'
-it('logs requests', async () => {
-    const logs: string[] = []
-    const events: { [event: string]: () => any } = {}
-    const log = pino(
-        {
-            level: 'debug',
-            timestamp: false,
-            base: {},
-        },
-        {
-            write(msg) {
-                logs.push(msg)
+describe('Other logging tests', () => {
+    it('logs requests', async () => {
+        const logs: string[] = []
+        const events: { [event: string]: () => any } = {}
+        const log = pino(
+            {
+                level: 'debug',
+                timestamp: false,
+                base: {},
             },
-        },
-    )
-    const middleware = expressRequestLoggingMiddleware(log, {
-        createRequestId() {
-            return '<requestid>'
-        },
-        now() {
-            return 12345
-        },
-    })
-    const res = {
-        on(event: string, cb: () => any) {
-            events[event] = cb.bind(res)
-        },
-        removeListener(event: string) {
-            delete events[event]
-        },
-    }
-    middleware(
-        {
-            header() {
-                return undefined
+            {
+                write(msg) {
+                    logs.push(msg)
+                },
             },
-            query: {},
-        } as any,
-        res as any,
-        () => {},
-    )
-    events.finish()
-
-    expect(logs).toMatchSnapshot()
-    expect(Object.keys(events).length).toBe(0)
-})
-
-it('logs API caller headers', async () => {
-    const logs: string[] = []
-    const events: { [event: string]: () => any } = {}
-    const log = pino(
-        {
-            level: 'debug',
-            timestamp: false,
-            base: {},
-        },
-        {
-            write(msg) {
-                logs.push(msg)
+        )
+        const middleware = expressRequestLoggingMiddleware(log, {
+            createRequestId() {
+                return '<requestid>'
             },
-        },
-    )
-    const middleware = expressRequestLoggingMiddleware(log, {
-        createRequestId() {
-            return '<requestid>'
-        },
-        now() {
-            return 12345
-        },
-    })
-    const res = {
-        on(event: string, cb: () => any) {
-            events[event] = cb.bind(res)
-        },
-        removeListener(event: string) {
-            delete events[event]
-        },
-    }
-    middleware(
-        {
-            header(name: string) {
-                if (name === 'x-request-id') {
-                    return 'testRequest1'
-                }
-                if (name === 'Caller') {
-                    return 'custom API caller'
-                }
-                return undefined
+            now() {
+                return 12345
             },
-            query: {},
-        } as any,
-        res as any,
-        () => {},
-    )
-    events.finish()
-
-    expect(logs).toMatchSnapshot()
-    expect(Object.keys(events).length).toBe(0)
-})
-
-it('logs requests when accounting for samplingOverrides', async () => {
-    const logs: string[] = []
-    const events: { [event: string]: () => any } = {}
-    const log = pino(
-        {
-            level: 'debug',
-            timestamp: false,
-            base: {},
-        },
-        {
-            write(msg) {
-                logs.push(msg)
+        })
+        const res = {
+            on(event: string, cb: () => any) {
+                events[event] = cb.bind(res)
             },
-        },
-    )
-    const middleware = expressRequestLoggingMiddleware(log, {
-        createRequestId() {
-            return '<requestid>'
-        },
-        now() {
-            return 12345
-        },
-        loggerSamplingOverride: {
-            'app-login': {
-                debugPercentage: 50,
-                infoPercentage: 50,
-            }
-        },
-        sampling: {
-            infoPercentage: 0,
-            debugPercentage: 0
+            removeListener(event: string) {
+                delete events[event]
+            },
         }
+        middleware(
+            {
+                header() {
+                    return undefined
+                },
+                query: {},
+            } as any,
+            res as any,
+            () => {},
+        )
+        events.finish()
+
+        expect(logs).toMatchSnapshot()
+        expect(Object.keys(events).length).toBe(0)
     })
-    const res = {
-        on(event: string, cb: () => any) {
-            events[event] = cb.bind(res)
-        },
-        removeListener(event: string) {
-            delete events[event]
-        },
-    }
-    middleware(
-        {
-            header(name: string) {
-                if (name === 'x-request-id') {
-                    return 'testRequest1'
-                }
-                if (name === 'Caller') {
-                    return 'custom API caller'
-                }
-                return undefined
+
+    it('logs API caller headers', async () => {
+        const logs: string[] = []
+        const events: { [event: string]: () => any } = {}
+        const log = pino(
+            {
+                level: 'debug',
+                timestamp: false,
+                base: {},
             },
-            query: {},
-            baseUrl: '/',
-            path: 'app-login/'
-        } as any,
-        res as any,
-        () => {},
-    )
-    events.finish()
-
-    expect(logs).toMatchSnapshot()
-    expect(Object.keys(events).length).toBe(0)
-})
-
-
-it('logs failed', async () => {
-    const logs: string[] = []
-    const events: { [event: string]: () => any } = {}
-    const log = pino(
-        {
-            level: 'debug',
-            timestamp: false,
-            base: {},
-        },
-        {
-            write(msg) {
-                logs.push(msg)
+            {
+                write(msg) {
+                    logs.push(msg)
+                },
             },
-        },
-    )
-    const middleware = expressRequestLoggingMiddleware(log, {
-        createRequestId() {
-            return '<requestid>'
-        },
-        now() {
-            return 12345
-        },
+        )
+        const middleware = expressRequestLoggingMiddleware(log, {
+            createRequestId() {
+                return '<requestid>'
+            },
+            now() {
+                return 12345
+            },
+        })
+        const res = {
+            on(event: string, cb: () => any) {
+                events[event] = cb.bind(res)
+            },
+            removeListener(event: string) {
+                delete events[event]
+            },
+        }
+        middleware(
+            {
+                header(name: string) {
+                    if (name === 'x-request-id') {
+                        return 'testRequest1'
+                    }
+                    if (name === 'Caller') {
+                        return 'custom API caller'
+                    }
+                    return undefined
+                },
+                query: {},
+            } as any,
+            res as any,
+            () => {},
+        )
+        events.finish()
+
+        expect(logs).toMatchSnapshot()
+        expect(Object.keys(events).length).toBe(0)
     })
-    const res = {
-        on(event: string, cb: () => any) {
-            events[event] = cb.bind(res)
-        },
-        removeListener(event: string) {
-            delete events[event]
-        },
-    }
-    middleware(
-        {
-            header() {
-                return undefined
-            },
-            query: {},
-        } as any,
-        res as any,
-        () => {},
-    )
-    events.error()
 
-    expect(logs).toMatchSnapshot()
-    expect(Object.keys(events).length).toBe(0)
-})
-
-it('logs close', async () => {
-    const logs: string[] = []
-    const events: { [event: string]: () => any } = {}
-    const log = pino(
-        {
-            level: 'debug',
-            timestamp: false,
-            base: {},
-        },
-        {
-            write(msg) {
-                logs.push(msg)
+    it('logs failed', async () => {
+        const logs: string[] = []
+        const events: { [event: string]: () => any } = {}
+        const log = pino(
+            {
+                level: 'debug',
+                timestamp: false,
+                base: {},
             },
-        },
-    )
-    const middleware = expressRequestLoggingMiddleware(log, {
-        createRequestId() {
-            return '<requestid>'
-        },
-        now() {
-            return 12345
-        },
+            {
+                write(msg) {
+                    logs.push(msg)
+                },
+            },
+        )
+        const middleware = expressRequestLoggingMiddleware(log, {
+            createRequestId() {
+                return '<requestid>'
+            },
+            now() {
+                return 12345
+            },
+        })
+        const res = {
+            on(event: string, cb: () => any) {
+                events[event] = cb.bind(res)
+            },
+            removeListener(event: string) {
+                delete events[event]
+            },
+        }
+        middleware(
+            {
+                header() {
+                    return undefined
+                },
+                query: {},
+            } as any,
+            res as any,
+            () => {},
+        )
+        events.error()
+
+        expect(logs).toMatchSnapshot()
+        expect(Object.keys(events).length).toBe(0)
     })
-    const res = {
-        on(event: string, cb: () => any) {
-            events[event] = cb.bind(res)
-        },
-        removeListener(event: string) {
-            delete events[event]
-        },
-    }
-    middleware(
-        {
-            header() {
-                return undefined
-            },
-            query: {},
-        } as any,
-        res as any,
-        () => {},
-    )
-    events.close()
 
-    expect(logs).toMatchSnapshot()
-    expect(Object.keys(events).length).toBe(0)
+    it('logs close', async () => {
+        const logs: string[] = []
+        const events: { [event: string]: () => any } = {}
+        const log = pino(
+            {
+                level: 'debug',
+                timestamp: false,
+                base: {},
+            },
+            {
+                write(msg) {
+                    logs.push(msg)
+                },
+            },
+        )
+        const middleware = expressRequestLoggingMiddleware(log, {
+            createRequestId() {
+                return '<requestid>'
+            },
+            now() {
+                return 12345
+            },
+        })
+        const res = {
+            on(event: string, cb: () => any) {
+                events[event] = cb.bind(res)
+            },
+            removeListener(event: string) {
+                delete events[event]
+            },
+        }
+        middleware(
+            {
+                header() {
+                    return undefined
+                },
+                query: {},
+            } as any,
+            res as any,
+            () => {},
+        )
+        events.close()
+
+        expect(logs).toMatchSnapshot()
+        expect(Object.keys(events).length).toBe(0)
+    })
 })
